e7c169525a1db564f86ee114660233931ebd1a65
BK172 <bk-172-v@yandex.ru>
2021-01-18 16:03:19 +0300

    Доработки теста App

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 jest.mock('./components/header/header', () => ({ Header: () => <div>Header component</div> }));
 jest.mock('./components/pages/login/login', () => ({ Login: () => <div>Login component</div> }));
+jest.mock('./components/pages/map/map', () => ({ Map: () => <div>Map component</div> }));
+jest.mock('./components/pages/profile/profile', () => ({ Profile: () => <div>Profile component</div> }));
 
 describe('App component test', () => {
   it('Should render Login correctly', () => {
@@ -14,5 +16,26 @@ describe('App component test', () => {
         <section><div>Login component</div></section>
       </main>
     `);
+  });
+
+  describe('Click on navigation buttons test', () => {
+    it('Should open the corresponding page', () => {
+      const { getByText, container } = render(<App />);
+
+      fireEvent.click(getByText('Map'));
+      expect(container.innerHTML).toMatchInlineSnapshot(`
+        <div>Header component</div>
+        <main>
+          <section><div>Map component</div></section>
+        </main>
+      `);
+      fireEvent.click(getByText('Profile'));
+      expect(container.innerHTML).toMatchInlineSnapshot(`
+        <div>Header component</div>
+        <main>
+          <section><div>Profile component</div></section>
+        </main>
+      `);
+    });
   });
 });
