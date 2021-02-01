699544e22f197084f64f369d740e7ce9b24d156c
BK172 <bk-172-v@yandex.ru>
2021-02-01 11:32:55 +0300

    Подключил карту

--- src/components/pages/map/map.jsx
+++ src/components/pages/map/map.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
 import Header from '../../header/header';
 
-const Map = ({onPageChange}) => {
-  return (
-    <div className="wrapper">
-      <Header onPageChange={onPageChange} />
-      <main className="main">
-        <section>
-          <p>Map</p>
-        </section>
-      </main>
-    </div>
-  );
-};
+class Map extends Component {
+  map = null;
+  mapContainer = React.createRef();
+
+  componentDidMount() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGlzZWx0dXJib2Rpc2VsIiwiYSI6ImNra21iYjFpejM2ZDEydnBhMnR6amp3czkifQ.mdeQS7AedbRhBQr2h8CC5A';
+
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [30.3056504, 59.9429126],
+      zoom: 10,
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  render() {
+    const {onPageChange} = this.props.onPageChange;
+
+    return (
+      <div className="wrapper">
+        <Header onPageChange={onPageChange} />
+        <main className="main">
+          <section className="map__wrapper">
+            <div className="map__map" data-testid="map" ref={this.mapContainer}></div>
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
 
 export default Map;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
-import { theme } from "loft-taxi-mui-theme";
-import { MuiThemeProvider } from "@material-ui/core/styles";
+import { theme } from 'loft-taxi-mui-theme';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 ReactDOM.render(
   <React.StrictMode>
