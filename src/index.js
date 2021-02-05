97c88f3e788fad4235ef05315d6f4fca00438a30
BK172 <bk-172-v@yandex.ru>
2021-02-05 10:57:20 +0300

    Вынес отдельно стили для mui

--- src/index.js
+++ src/index.js
@@ -4,85 +4,8 @@ import { AuthProvider } from './components/auth-context/auth-context';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { createMuiTheme } from "@material-ui/core/styles";
-import orange from "@material-ui/core/colors/orange";
+import theme from "./mui-theme.js"
 import './index.css';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#ffc617"
-    },
-    secondary: {
-      main: "#ff0000"
-    }
-  },
-  status: {
-    danger: orange[500]
-  },
-  overrides: {
-    MuiFormLabel: {
-      root: {
-        "&$focused": {
-          color: "#828282"
-        }
-      }
-    },
-    MuiLink: {
-      root: {
-        color: "#fdbf5a",
-        cursor: "pointer"
-      },
-      underlineHover: {
-        "&:hover": {
-          textDecoration: "none"
-        }
-      }
-    },
-    MuiInput: {
-      underline: {
-        "&:after": {
-          "border-bottom-color": "#1473e6"
-        }
-      }
-    },
-    MuiFormHelperText: {
-      root: {
-        position: "absolute",
-        bottom: "-1.2em"
-      }
-    },
-    MuiButton: {
-      root: {
-        textTransform: 'none',
-      },
-      containedPrimary: {
-        borderRadius: "40px",
-        color: "#000000de",
-        fontWeight: "400",
-        letterSpacing: "0px",
-        backgroundColor: "#fdbf5a",
-        "&:hover": {
-          backgroundColor: "#ffa842"
-        },
-        "&:focus": {
-          boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)"
-        }
-      },
-      containedSizeLarge: {
-        fontSize: "1.3rem"
-      }
-    },
-    MuiTypography: {
-      h4: {
-        marginBottom: "2.75rem",
-        fontSize: "1.65rem",
-        textAlign: "center",
-        fontWeight: "700"
-      }
-    }
-  }
-});
 
 ReactDOM.render(
   <React.StrictMode>
