db4b2cb7f2919874b0ff2d847f85eb759477a934
BK172 <bk-172-v@yandex.ru>
2021-02-08 01:25:52 +0300

    Header переписан ф-ым компонентом

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,40 +4,28 @@ import { AuthContext } from '../auth-context/auth-context';
 import { AppBar, Toolbar, Button } from '@material-ui/core';
 import logo from '../../assets/images/logo-header.svg';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
+const Header = ({ onPageChange }) => {
+  const ctx = React.useContext(AuthContext);
 
-    this.onLogOut = this.onLogOut.bind(this);
-  }
+  const onLogOut = () => {
+    onPageChange('login');
+    ctx.logOut();
+  };
 
-  onLogOut = () => {
-    this.props.onPageChange('login');
-
-    const { logOut } = this.context;
-    logOut();
-  }
-
-  static contextType = AuthContext;
-
-  render() {
-    const { onPageChange } = this.props;
-
-    return (
-      <AppBar position="static" elevation={4}>
-        <Toolbar classes={{ root: 'header' }}>
-          <div className="header__logo">
-            <img src={logo} alt="LoftTaxi logo"/>
-          </div>
-          <Button classes={{ root: 'header__button' }} type="button" color="primary" onClick={() => onPageChange('map')}>Карта</Button>
-          <Button classes={{ root: 'header__button' }} type="button" color="inherit" onClick={() => onPageChange('profile')}>Профиль</Button>
-          <Button classes={{ root: 'header__button' }} type="button" color="inherit" onClick={() => onPageChange('login')}>Войти</Button>
-          <Button classes={{ root: 'header__button' }} type="button" color="inherit" onClick={() => this.onLogOut()}>Выйти</Button>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+  return (
+    <AppBar position="static" elevation={4}>
+      <Toolbar classes={{ root: 'header' }}>
+        <div className="header__logo">
+          <img src={logo} alt="LoftTaxi logo"/>
+        </div>
+        <Button classes={{ root: 'header__button' }} type="button" color="primary" onClick={() => onPageChange('map')}>Карта</Button>
+        <Button classes={{ root: 'header__button' }} type="button" color="inherit" onClick={() => onPageChange('profile')}>Профиль</Button>
+        <Button classes={{ root: 'header__button' }} type="button" color="inherit" onClick={() => onPageChange('login')}>Войти</Button>
+        <Button classes={{ root: 'header__button' }} type="button" color="inherit" onClick={() => onLogOut()}>Выйти</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 Header.propTypes = {
   onPageChange: PropTypes.func.isRequired,
--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -3,23 +3,11 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import Header from './header';
 
-const mockContext = jest.fn();
-jest.mock('../auth-context/auth-context', () => ({
-  Consumer: ({ children }) => children(mockContext()),
-}));
-
 describe('Header component test', () => {
-  beforeAll(() => {
-    mockContext.mockClear();
-  });
-
   it('Should render Header correctly', () => {
     const tree = renderer.create(
       <Header
         onPageChange={jest.fn()}
-        logIn={jest.fn()}
-        logOut={jest.fn()}
-        isLoggedIn={true}
       />
     ).toJSON();
     expect(tree).toMatchSnapshot();
@@ -30,9 +18,6 @@ describe('Header component test', () => {
     const wrapper = shallow(
         <Header
           onPageChange={jest.fn()}
-          logIn={jest.fn()}
-          logOut={jest.fn()}
-          isLoggedIn={true}
         />
     );
 
