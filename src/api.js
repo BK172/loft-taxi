b9748c9b9e80526d6d0c18bccb6e891997c22d16
BK172 <bk-172-v@yandex.ru>
2021-02-10 23:48:08 +0300

    Добавлены функции для api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,34 @@
 export const serverLogin = async (email, password) => {
-  return fetch(
-    `https://loft-taxi.glitch.me/auth?username=${email}&password=${password}`
-  )
-    .then(res => res.json())
-    .then(data => data.success);
+  return axios('https://loft-taxi.glitch.me/auth', {
+    method: 'post',
+    params: {
+      email,
+      password,
+    },
+  }).then(response => response.data.success);
+};
+
+export const serverRegister = async (email, password, userName, userSurname) => {
+  return axios('https://loft-taxi.glitch.me/auth', {
+    method: 'post',
+    params: {
+      email,
+      password,
+      name: userName,
+      surname: userSurname,
+    },
+  }).then(response => response.data.success);
+};
+
+export const addCard = async (expiryDate, cardNumber, cardName, token, cvc) => {
+  return axios('https://loft-taxi.glitch.me/cart', {
+    method: 'post',
+    params: {
+      expiryDate,
+      cardNumber,
+      cardName,
+      token,
+      cvc,
+    },
+  }).then(response => response.data.success);
 };
