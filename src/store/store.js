5105f591eeba44be2fa45670d27858ba1b76d28b
BK172 <bk-172-v@yandex.ru>
2021-02-11 14:39:15 +0300

    Добавил redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -2,15 +2,21 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/root-reducer';
 import { authMiddleware } from './middlewares/auth';
 import { persistStore, persistReducer } from 'redux-persist';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['auth'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const store = createStore(persistedReducer, applyMiddleware(authMiddleware));
+export const store = createStore(persistedReducer,
+  composeWithDevTools(
+    applyMiddleware(authMiddleware),
+  )
+);
 
 export const persistor = persistStore(store);
