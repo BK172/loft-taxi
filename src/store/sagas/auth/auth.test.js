aed9b058d0d960d00e41439cdc63e43afade5bf5
BK172 <bk-172-v@yandex.ru>
2021-02-18 02:07:01 +0300

    Тест для auth saga

---
+++ src/store/sagas/auth/auth.test.js
@@ -0,0 +1,29 @@
+import { authenticateSaga } from './auth';
+import { auth, ActionType } from '../../reducers/auth/actions';
+import { recordSaga } from './recordSaga';
+
+jest.mock('../../../api', () => ({ serverLogin: jest.fn(() => true) }));
+
+describe('AuthSaga test', () => {
+  describe('#AUTHENTICATE', () => {
+    it('authenticates through api', async () => {
+      const dispatched = await recordSaga(
+        authenticateSaga,
+        auth('test@test.com', '123123')
+      );
+
+      expect(dispatched).toEqual([
+        {
+          type: ActionType.LOG_IN,
+        },
+        {
+          type: ActionType.SAVE_AUTH_TOKEN,
+          payload: {
+            email: 'test@test.com',
+            password: '123123',
+          },
+        },
+      ]);
+    });
+  });
+});
